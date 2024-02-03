47f806e8edd6116ac848cee10de5a714bcbf4bdd
Tomasz <n1kula@wp.pl>
2024-02-03 16:51:10 +0100

    cars list with extension syntax

--- FuelConsumption/Car.cs
+++ FuelConsumption/Car.cs
@@ -1,15 +1,33 @@
-﻿namespace FuelConsumption
+﻿using System;
+
+namespace FuelConsumption
 {
     public class Car
     { 
         public int Year { get; set; }
         public string Producent { get; set; }
         public string Model { get; set; }
-        public double EngSize { get; set; }
+        public string EngSize { get; set; }
         public int Cylinders { get; set; }
         public int CityFuelConsumption { get; set; }
         public int MotorwayFuelConsumption { get; set; }
         public int MixedFuelConsumption { get; set; }
-        
+
+        internal static Car ParseCSV(string line)
+        {
+            var columns = line.Split(',');
+
+            return new Car
+            {
+                Year = int.Parse(columns[0]),
+                Producent = columns[1],
+                Model = columns[2],
+                EngSize = columns[3],
+                Cylinders = int.Parse(columns[4]),
+                CityFuelConsumption = int.Parse(columns[5]),
+                MotorwayFuelConsumption = int.Parse(columns[6]),
+                MixedFuelConsumption = int.Parse(columns[7])
+            };
+        }
     }
 }
--- FuelConsumption/Program.cs
+++ FuelConsumption/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FuelConsumption
 {
@@ -10,6 +9,21 @@ namespace FuelConsumption
     {
         static void Main(string[] args)
         {
+            var cars = ReadFile("fuelConsumption.csv");
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Producent + car.Model);
+            }
+        }
+
+        private static List<Car> ReadFile(string path)
+        {
+            return File.ReadAllLines(path)
+                        .Skip(1)
+                        .Where(line => line.Length > 1)
+                        .Select(Car.ParseCSV)
+                        .ToList();
         }
     }
 }
