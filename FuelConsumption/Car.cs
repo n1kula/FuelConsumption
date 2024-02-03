8f1ddcf40303fe6a2bda9ab2fd9f45c95ccf017d
Tomasz <n1kula@wp.pl>
2024-02-03 16:50:43 +0100

    Car Class and fuel csv data added

---
+++ FuelConsumption/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace FuelConsumption
+{
+    public class Car
+    { 
+        public int Year { get; set; }
+        public string Producent { get; set; }
+        public string Model { get; set; }
+        public double EngSize { get; set; }
+        public int Cylinders { get; set; }
+        public int CityFuelConsumption { get; set; }
+        public int MotorwayFuelConsumption { get; set; }
+        public int MixedFuelConsumption { get; set; }
+        
+    }
+}
